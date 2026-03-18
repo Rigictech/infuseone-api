3970b0f92d9854a82bc7d6825918b24f69e5eb49
ronak-developer <developer+ronak@rigicglobalsolutions.com>
2026-03-18 14:56:02 +0530

    Update app.php

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Infuse One'),
-    'frontend_login_url'=>env('FRONT_REDIRECTION_MAIN_LOGIN_URL','https://rigicgspl.com/infuseone-ghost-page/'),
+    'frontend_login_url'=>env('FRONT_REDIRECTION_MAIN_LOGIN_URL','https://ghost-page.infuseone.com/'),
 
     /*
     |--------------------------------------------------------------------------
